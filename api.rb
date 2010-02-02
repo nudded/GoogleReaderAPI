946c31f53542becc34db0ec2ce03cfa4d2e1c305
Toon Willems <willemstoon@gmail.com>
2010-02-02 08:26:59 +0100

    created base api

---
+++ api.rb
@@ -0,0 +1,24 @@
+module GoogleReader
+
+  BASE_URL = "http://www.google.com/reader/api/0/"
+  
+  # the main api
+  class API
+    
+    require "user"
+    
+    # specify email and password in the arg hash
+    def initialize(opthash)
+      @user = GoogleReader::User.new(opthash)
+    end
+    
+    # get the unread count for the current user
+    def unread_count
+      link = GoogleReader::BASE_URL + "unread-count"
+      @user.get_request(link,:allcomments => true,:output => :json,:ck => Time.now.to_i)
+    end
+    
+  end
+  
+  
+end
