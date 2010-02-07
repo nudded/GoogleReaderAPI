2cf6bf04eb377d2920af189bee7376d176b3d7f3
Toon Willems <willemstoon@gmail.com>
2010-02-07 13:54:12 +0100

    removed useless test

--- api_test.rb
+++ api_test.rb
@@ -7,7 +7,5 @@ pass = gets.chomp
 puts
 
 api = GoogleReader::Api.new 'willemstoon@gmail.com', pass
-p api.unread_count
-p api.unread_count 'news.ycombinator.com'
 p api.user_info
 p api.subscriptions
