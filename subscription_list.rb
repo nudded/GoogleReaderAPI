77a6e75c1424e6c7cc2578495466f025c3571d79
Toon Willems <willemstoon@gmail.com>
2010-02-07 14:37:45 +0100

    subscriptionlist builds Feed objects

--- subscription_list.rb
+++ subscription_list.rb
@@ -2,11 +2,28 @@ module GoogleReader
   
   class SubscriptionList
     
+    require "feed"
+    
     def initialize(api)
       @api = api
+      update
     end
 
+    def feeds
+      @feeds
+    end
     
+    private
+    
+    def update
+      fetch_list
+    end
+    
+    def fetch_list
+      json = JSON[@api.get_link 'api/0/subscription/list', :output => :json]['subscriptions']
+
+      @feeds = json.map {|hash| GoogleReader::Feed.new(hash,@api) }
+    end
     
   end
   
