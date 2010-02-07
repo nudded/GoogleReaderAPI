ec6125d26ada051c278abe37c9219b60d134b75f
Toon Willems <willemstoon@gmail.com>
2010-02-07 14:37:09 +0100

    Feed stores the useful values, prints nicely

--- feed.rb
+++ feed.rb
@@ -1,8 +1,26 @@
 module GoogleReader
   class Feed
     
-    def initialize
+    attr_reader :url, :title
+    
+    def initialize(hash,api)
+      # strip the first 5 characters of the url (they are 'feed/')
+      @url = hash['id'][5..-1]
+      @title = hash['title']
+      # no idea what this is used for
+      @sortid = hash['sortid']
+      @categories = hash['categories']
+      @firstitemmsec = hash['firstitemmsec']
       
+      @api = api
+    end
+    
+    def inspect
+      to_s
+    end
+    
+    def to_s
+      "<<Feed: #{title} url:#{url}>>"
     end
     
   end
