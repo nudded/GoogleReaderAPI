fdd636207ad27b88bf5ed2c095b3f973d9a51d80
Toon Willems <willemstoon@gmail.com>
2010-02-10 22:46:07 +0100

    added read_items, starred_items, items to feed

--- feed.rb
+++ feed.rb
@@ -29,6 +29,19 @@ module GoogleReader
       entry = JSON[@api.cached_unread_count]['unreadcounts'].find {|h| h['id'] == "feed/#{url}"}
       entry ? entry['count'] : 0
     end
+    # return count read items
+    def read_items(count=20)
+      create_entries get_user_items('read',:n => count)
+    end
+    
+    def starred_items(count=20)
+      create_entries get_user_items('starred',:n => count)
+    end
+    
+    # return the number of specified items. (read or not)
+    def items(count = 20)
+      create_entries get_feed_items(:n => count)
+    end
     
     # return all the unread items in an array
     def all_unread_items
@@ -39,8 +52,7 @@ module GoogleReader
     # will try to return the amount of unread items you specify. unless there are no more.
     # will return 20 unread items by default.
     def unread_items(count = 20)
-      atom_feed = @api.get_link "atom/feed/#{url}", :n => count, :xt => 'user/-/state/com.google/read', :c => @continuation
-      RSS::Parser.parse(atom_feed).entries.map {|e| Entry.new(@api,e) }
+      create_entries get_feed_items(:n => count,:xt => 'user/-/state/com.google/read')
     end
     
     def inspect
@@ -51,5 +63,20 @@ module GoogleReader
       "<<Feed: #{title} url:#{url}>>"
     end
     
+    private
+    
+    def get_user_items(state,args={})
+      @api.get_link "atom/user/-/state/com.google/#{state}" , args
+    end
+    def get_feed_items(args={})
+      @api.get_link "atom/feed/#{url}" , args
+    end
+    
+    def create_entries(atom_feed)
+      RSS::Parser.parse(atom_feed).entries.map {|e| Entry.new(@api,e) }
+    end
+    
+    
   end
+  
 end
