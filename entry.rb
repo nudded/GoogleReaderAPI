f774e712bc458552e0eeea4685a5268710c8c1e4
Toon Willems <willemstoon@gmail.com>
2010-02-10 09:24:21 +0100

    added Feed::Entry class

---
+++ entry.rb
@@ -0,0 +1,33 @@
+module GoogleReader
+  class Feed
+    class Entry
+    
+      attr_reader :entry
+    
+      def initialize(api,entry)
+        @api, @entry = api, entry
+      end
+    
+      def toggle_read
+        edit_tag 'user/-/state/com.google/read'
+      end
+    
+      def toggle_like
+        edit_tag 'user/-/state/com.google/like'
+      end
+    
+      def toggle_star
+        edit_tag 'user/-/state/com.google/starred'
+      end
+    
+      private 
+    
+      def edit_tag(tag_identifier)
+        @api.post_link "api/0/edit-tag" , :a => tag_identifier ,
+                                          :s => entry.parent.id.content.to_s.scan(/feed\/.*/) ,
+                                          :i => entry.id.content.to_s
+      end
+    
+    end
+  end
+end
