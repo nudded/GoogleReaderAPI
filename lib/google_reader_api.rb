b5729782feee0ff2d18a4d90c71709e2b8cae1fc
Toon Willems <willemstoon@gmail.com>
2010-02-21 12:47:11 +0100

    more cleanup

---
+++ lib/google_reader_api.rb
@@ -0,0 +1,7 @@
+require 'google-reader-api/api'
+require "google-reader-api/cache"
+require "google-reader-api/entry"
+require "google-reader-api/feed"
+require "google-reader-api/rss_utils"
+require "google-reader-api/subscription_list"
+require "google-reader-api/user"
